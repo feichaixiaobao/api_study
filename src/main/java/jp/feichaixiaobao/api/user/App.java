26075c80b6c654777a3d078b3601888736975040
feichaixiaobao <feichaixiaobao@gmail.com>
2023-01-21 14:07:15 +0900

    init

---
+++ src/main/java/jp/feichaixiaobao/api/user/App.java
@@ -0,0 +1,13 @@
+package jp.feichaixiaobao.api.user;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
