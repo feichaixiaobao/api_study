eb3a578aff2ad73e92507937ffe22cf0b3db08bf
vina <code.tokyo@qq.com>
2023-01-22 18:41:16 +0800

    1.Modify App.java 2.Add HomeController.java / home.html

--- src/main/java/jp/feichaixiaobao/api/user/vina/App.java
+++ src/main/java/jp/feichaixiaobao/api/user/vina/App.java
@@ -1,7 +1,22 @@
 package jp.feichaixiaobao.api.user.vina;
 
-public class App {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App  {
+
     public static void main(String[] args) {
-        System.out.println("Hello World !!!");
+        // 启动springboot
+        SpringApplication.run(App.class, args);
     }
+
 }
+
+
+
+//public class App {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World !!!");
+//    }
+//}
---
+++ src/main/java/jp/feichaixiaobao/api/user/vina/controller/HomeController.java
@@ -0,0 +1,14 @@
+package jp.feichaixiaobao.api.user.vina.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    String home(){
+        return "home";
+    }
+}
