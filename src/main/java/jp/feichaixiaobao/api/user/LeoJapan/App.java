ed246f9e6e9f3176c72d752720ca4d26f5a139a7
LeoJapan <liangchuanqi@gmail.com>
2023-02-03 21:09:10 +0800

    Springboot启动类追加

---
+++ src/main/java/jp/feichaixiaobao/api/user/LeoJapan/App.java
@@ -0,0 +1,20 @@
+package jp.feichaixiaobao.api.user.LeoJapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:起動クラス
+ * @version v1.0
+ * @author LeoJapan
+ * @date 2023年1月26日 
+ */
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);       
+    }
+}
